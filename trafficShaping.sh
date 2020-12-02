8874f9e8c1a3d6d681c820175022833ac45b3a4f
Priya Shah <shahpriya890@gmail.com>
2020-12-02 17:44:15 -0500

    Update

--- trafficShaping.sh
+++ trafficShaping.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-TC = /sbin/tc
-IFACE = enp0s3
-LIMIT = 100mbps
+TC=/sbin/tc
+IFACE=ens33
+LIMIT=100mbps
 
-DST_CDIR = 10.0.2.15/32
-DST_CDIR2 = 1.1.1.1/32
+DST_CDIR=127.0.0.1/32
+DST_CDIR2=1.0.0.1/32
 
-real="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
-dummy="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
+echo "Working"
 
 makeQdiscTree () {
     # root
-    $TC qdisc add dev $IFACE root handle 1:0 htb 
+    $TC qdisc add dev $IFACE root handle 1: htb default 30
 
-    # child 1 - real
+    # parent
     $TC class add dev $IFACE parent 1:0 classid 1:1 htb rate $LIMIT
 
+    # child 1 - real
+    $TC class add dev $IFACE parent 1:1 classid 1:2 htb ceil $LIMIT rate 1mbps
+
     # child 2 - dummy
-    $TC class add dev $IFACE parent 1:0 classid 1:2 htb rate $LIMIT
+    $TC class add dev $IFACE parent 1:1 classid 1:3 htb rate $LIMIT
     
     # filter
-    $U3 match ip dst $DST_CDIR flowid 1:1
-    $U3 match ip dst $DST_CDIR2 flowid 1:2
+    $TC filter add dev $IFACE protocol ip parent 1:0 prio 1 u32 match ip dst $DST_CDIR flowid 1:2
+    $TC filter add dev $IFACE protocol ip parent 1:0 prio 1 u32 match ip dst $DST_CDIR2 flowid 1:3
+    
 }
 
 erasePrev () {
@@ -31,3 +34,6 @@ erasePrev () {
 erasePrev
 makeQdiscTree
 
+echo "Done"
+
+#iperf3
